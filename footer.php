c34ddc39a23826f7703f5ce5a549e8c5bba9de0b
ttwei3 <139569407+ttwei3@users.noreply.github.com>
2023-11-14 12:07:33 -0600

    Create footer.php
    It is designed to offer essential information and navigation links like contact details, copyright, and social media connections for the website's users.

---
+++ footer.php
@@ -0,0 +1,30 @@
+<!-- footer.php -->
+<footer>
+    <img src="" alt="" id="footerlogo">
+    <div class="footer-content">
+        <div class="footer-nav">
+            <ul>
+                <li>Copyright © 2006&nbsp;<a href="#">Naian Bearing</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="product.php">Products</a></li>
+                <li><a href="#">Support</a></li>
+            </ul>
+        </div>
+        <div class="footer-under">
+            <div class="contact-info">
+                <h3>Contact Us</h3>
+                <p>Email: naian@gmail.com</p>
+            </div>
+            <div class="social-media">
+                <h3>Follow Us</h3>
+                <img src="images/ins.svg" alt="fail" id="ins">
+                <img src="images/tw.svg" alt="fail" id="tw">
+                <img src="images/ytb.svg" alt="fail" id="ytb">
+            </div>
+        </div>
+        <div id="indexsocial">
+        </div>
+    </div>
+</footer>
+</body>
+</html>
