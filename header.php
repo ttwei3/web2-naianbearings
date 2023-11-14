ffac3050c372b1388cee3fce67d894e9fe78a98e
ttwei3 <139569407+ttwei3@users.noreply.github.com>
2023-11-14 12:06:11 -0600

    Create header.php
    It is for the header section of a website, designed to display a welcome message, manage user login sessions, provide navigation links to different pages like home, products, and support, and include a search bar for product searches.

---
+++ header.php
@@ -0,0 +1,38 @@
+<!-- header.php-->
+<?php 
+    session_start(); 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Website</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+<header>
+    <div class="top-header">
+    <div class="login-info">
+        <span>Welcome to Naian Bearing!</span>
+        <?php if (isset($_SESSION['user_id'])): ?>
+            <a href="logout.php">Log out</a>
+            <?php if (isset($_SESSION['is_admin']) && $_SESSION['is_admin']): ?>
+                <a href="admin.php">Dashboard</a>
+            <?php endif; ?>
+        <?php else: ?>
+            <a href="login.php">Log in</a>
+        <?php endif; ?>
+    </div>
+        </div>
+        <div class="search-bar">
+            <input type="text" placeholder="Search our product">
+            <button>Search</button>
+        </div>
+    </div>
+    <nav>
+        <ul class="main-nav">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="product.php">Products</a></li>
+            <li><a href="support.php">Support</a></li>
+        </ul>
+    </nav>
+</header>
