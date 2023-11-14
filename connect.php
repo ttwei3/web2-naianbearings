18f51112a4bbf5a745eb115e24b74ae6ef6b823c
ttwei3 <139569407+ttwei3@users.noreply.github.com>
2023-11-14 11:57:09 -0600

    Create connect.php
    The connect.php used to establish a connection to a MySQL database using PHP PDO extension.

---
+++ connect.php
@@ -0,0 +1,20 @@
+ <!-- connect.php-->
+ <?php
+     define('DB_DSN','mysql:host=localhost;dbname=project;charset=utf8');
+     define('DB_USER','tingting');
+     define('DB_PASS','tt219219');     
+     
+    //  PDO is PHP Data Objects
+    //  mysqli <-- BAD. 
+    //  PDO <-- GOOD.
+     try {
+         // Try creating new PDO connection to MySQL.
+         $db = new PDO(DB_DSN, DB_USER, DB_PASS);
+         //,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+     } catch (PDOException $e) {
+         print "Error: " . $e->getMessage();
+         die(); // Force execution to stop on errors.
+         // When deploying to production you should handle this
+         // situation more gracefully. ¯\_(ツ)_/¯
+     }
+ ?>
