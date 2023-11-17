5e935b947acc0ae082ecdb3cad6cb48e24305365
ttwei3 <139569407+ttwei3@users.noreply.github.com>
2023-11-17 08:42:04 -0600

    Update delete_product.php
    When deleting a product, delete the image associated with the product at the same time, including checking and deleting the image record associated with the product and the actual image file in the file system before deleting the product record.

--- delete_product.php
+++ delete_product.php
@@ -1,11 +1,28 @@
-<!-- delete_product.php-->
 <?php
 require('connect.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $product_id = $_POST['product_id'] ?? null;
-    
+
     if ($product_id) {
+        $imageQuery = "SELECT filename FROM Images WHERE product_id = :product_id";
+        $imageStmt = $db->prepare($imageQuery);
+        $imageStmt->bindParam(':product_id', $product_id, PDO::PARAM_INT);
+        $imageStmt->execute();
+        $imageRow = $imageStmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($imageRow) {
+            $imagePath = 'upload/' . $imageRow['filename'];
+            if (file_exists($imagePath)) {
+                unlink($imagePath); 
+            }
+
+            $removeImageQuery = "DELETE FROM Images WHERE product_id = :product_id";
+            $removeImageStmt = $db->prepare($removeImageQuery);
+            $removeImageStmt->bindParam(':product_id', $product_id, PDO::PARAM_INT);
+            $removeImageStmt->execute();
+        }
+        
         $query = "DELETE FROM Products WHERE product_id = :product_id";
         $stmt = $db->prepare($query);
         $stmt->bindParam(':product_id', $product_id, PDO::PARAM_INT);
@@ -15,3 +32,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 header('Location: admin.php');
 exit;
+?>
