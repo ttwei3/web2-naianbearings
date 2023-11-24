3896f026af30cdeacadf29165e7f32026d3c5244
ttwei3 <139569407+ttwei3@users.noreply.github.com>
2023-11-23 23:22:31 -0600

    Update header_admin.php
    Changed the order

--- header_admin.php
+++ header_admin.php
@@ -25,10 +25,11 @@
     </div>
     <nav>
         <ul class="main-nav">
-            <li><a href="admin.php">DashBoard</a></li>
             <li><a href="index.php">Home</a></li>
-            <li><a href="product.php">Products</a></li>
-            <li><a href="support.php">Support</a></li>
+            <li><a href="admin.php">DashBoard</a></li>
+            <li><a href="manage_categories.php">Categories Management</a></li>
+            <li><a href="manage_products.php">Products Management</a></li>
+            <li><a href="manage_comments.php">Comments Management</a></li>
         </ul>
     </nav>
 </header>
