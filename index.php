36d831b19a1532914eb329399539d37147b86e89
ttwei3 <139569407+ttwei3@users.noreply.github.com>
2023-12-03 18:50:21 -0600

    Update index.php
    Improved the information on the homepage and added the why choose us module

--- index.php
+++ index.php
@@ -1,13 +1,22 @@
 <!-- index.php-->
 <?php
     session_start();
-    require('connect.php'); // 确保这里正确地引入了数据库连接文件
+    require('connect.php'); 
 
-// 获取所有可用类别
-$categoryQuery = "SELECT category_id, category_name FROM categories";
-$categoryStmt = $db->query($categoryQuery);
-$categories = $categoryStmt->fetchAll(PDO::FETCH_ASSOC);
+    if (isset($_SESSION['success_message'])) {
+        echo '<p class="success-message">' . $_SESSION['success_message'] . '</p>';
+        unset($_SESSION['success_message']); 
+    }
+
+    $categoryQuery = "SELECT category_id, category_name FROM categories";
+    $categoryStmt = $db->query($categoryQuery);
+    $categories = $categoryStmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $search_keyword = "";
+    $category_selected = "";
+    
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -50,17 +59,30 @@ $categories = $categoryStmt->fetchAll(PDO::FETCH_ASSOC);
             <ul class="main-nav">
                 <li><a href="#">Home</a></li>
                 <li><a href="product.php">Products</a></li>
-                <li><a href="#">Support</a></li>
             </ul>
         </nav>
     </header>
     <main>
-        <section class="content-section">
-            <h1>Welcome to Our Website</h1>
-            <p>At Naian Bearing, we pride ourselves on being a leading manufacturer and distributor of high-quality bearings, serving diverse industries with excellence since 2006. Our unwavering commitment to precision engineering, top-notch materials, and exceptional customer service has made us a trusted partner for businesses worldwide.</p>
-        </section>
-        <section class="products-section">
-            <h2>Our Products</h2>
+        <h1 class="h1">Welcome to Our Website</h1>
+        <p id="paragraph">At Naian Bearing, we pride ourselves on being a leading manufacturer and distributor of high-quality bearings, serving diverse industries with excellence since 2006. Our unwavering commitment to precision engineering, top-notch materials, and exceptional customer service has made us a trusted partner for businesses worldwide.</p>
+        <h1 class="h1">WHY CHOOSE US</h1>
+        <section id="whychooseus">
+            <div id="imgsp1">
+                <img src="images/image15.jpg" alt="fail to load" >
+                <p id="p1"><b>Unmatched Manufacturing Expertise:</b>Our bearings are crafted using cutting-edge manufacturing processes, supported by seasoned engineers with years of experience. We ensure every product meets the highest industry standards.</p>
+            </div>
+            <div id="imgsp2">
+                <img src="images/image14.jpg" alt="fail to load" >
+                <p id="p2"><b>Wide Product Range:</b>With an extensive catalog of bearings, we cater to various industrial applications, including automotive, industrial machinery, agriculture... Whether you need standard or custom-designed bearings, we have the right solution for you.</p>
+            </div>
+            <div id="imgsp3">
+                <img src="images/image13.jpg" alt="fail to load" >
+                <p id="p3"><b>Superior Quality Assurance:</b>Quality is at the core of everything in our company. Our company subject our bearings to rigorous testing and inspection processes to ensure exceed your expectations in performance, reliability, and durability.</p>
+            </div>
+            <div id="imgsp4">
+                <img src="images/image12.jpg" alt="fail to load" >
+                <p id="p4"><b>Customer-Centric Approach:</b>Your success is our success. Our company value our clients and work closely with them to understand their unique requirements, providing tailored solutions and personalized support to ensure maximum satisfaction.</p>
+            </div>
         </section>
     </main>
 
