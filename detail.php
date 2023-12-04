da93b1e79317d1b070ac2ddf8be95a9347e65e6f
ttwei3 <139569407+ttwei3@users.noreply.github.com>
2023-12-03 18:48:49 -0600

    anonymous and existing username
    Change the code to allow comments when the username is present and allow anonymous comments

--- detail.php
+++ detail.php
@@ -2,54 +2,41 @@
 <?php
 require('connect.php');
 require('header.php');
-ini_set('display_errors', 1);
+
+/* ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL); 
-$product_id = $_GET['id']; 
+error_reporting(E_ALL);  */
 
-// 检查是否有错误消息需要显示
+$product_id = $_GET['id'] ?? null;
+
 $error = isset($_SESSION['error']) ? $_SESSION['error'] : '';
-unset($_SESSION['error']); // 清除错误消息
+unset($_SESSION['error']);
 
-// 处理评论提交
 if (isset($_POST['submit_comment'])) {
 
     $comment_name = empty($_POST['comment_name']) ? 'Anonymous User' : $_POST['comment_name'];
     $comment_text = $_POST['comment_text'];
 
-    // 验证验证码
     if ($_POST['captcha'] !== $_SESSION['captcha']) {
         $_SESSION['error'] = "Invalid CAPTCHA. Please try again.";
         $_SESSION['saved_input'] = $_POST;
-        header('Location: detail.php?id=' . $product_id); // 重新加载页面
-        exit();
-    }
-
-    // 检查用户名是否已存在
-    $checkQuery = "SELECT * FROM Comments WHERE username = :username";
-    $stmt = $db->prepare($checkQuery);
-    $stmt->bindParam(':username', $comment_name);
-    $stmt->execute();
-
-    if ($stmt->rowCount() > 0) {
-        $_SESSION['error'] = "Username is already taken, please choose another username.";
-    } else {
-        // 插入评论到数据库
-        $insertQuery = "INSERT INTO Comments (product_id, user_id, comment_content, comment_date, username) VALUES (:product_id, :user_id, :comment_content, NOW(), :username)";
-        $stmt = $db->prepare($insertQuery);
-        $stmt->bindParam(':product_id', $product_id);
-        $stmt->bindParam(':user_id', $userId); // 确保这里 $userId 已经正确定义
-        $stmt->bindParam(':comment_content', $comment_text);
-        $stmt->bindParam(':username', $comment_name);
-        $stmt->execute();
-
-        // 重定向以避免表单重复提交
         header('Location: detail.php?id=' . $product_id);
         exit();
     }
+
+    $insertQuery = "INSERT INTO Comments (product_id, user_id, comment_content, comment_date, username) VALUES (:product_id, :user_id, :comment_content, NOW(), :username)";
+    $stmt = $db->prepare($insertQuery);
+    $stmt->bindParam(':product_id', $product_id);
+    $stmt->bindParam(':user_id', $userId); 
+    $stmt->bindParam(':comment_content', $comment_text);
+    $stmt->bindParam(':username', $comment_name);
+    $stmt->execute();
+
+    header('Location: detail.php?id=' . $product_id);
+    exit();
 }
 
-// 获取产品信息
+
 $query = "SELECT Products.*, Images.filename FROM Products LEFT JOIN Images ON Products.product_id = Images.product_id WHERE Products.product_id = :product_id";
 $stmt = $db->prepare($query);
 $stmt->bindParam(':product_id', $product_id);
@@ -59,9 +46,8 @@ $product = $stmt->fetch(PDO::FETCH_ASSOC);
 
 
     <?php if ($product): ?>
-        <!-- 产品详情 -->
         <h2><?php echo htmlspecialchars($product['product_name']); ?></h2>
-        <p><strong>Description:</strong> <?php echo strip_tags($product['product_description']) ?? 'No description'; ?></p>
+        <p><strong>Description:</strong> <?php echo $product['product_description'] ?? 'No description'; ?>
         <p><strong>Price: </strong>$<?php echo htmlspecialchars($product['product_price']); ?></p>
         <p><strong>Stock Quantity: </strong><?php echo htmlspecialchars($product['stock_quantity']); ?></p>
         <p><strong>Category ID: </strong><?php echo htmlspecialchars($product['category_id']); ?></p>
@@ -69,16 +55,13 @@ $product = $stmt->fetch(PDO::FETCH_ASSOC);
             <img src="upload/<?php echo htmlspecialchars($product['filename']); ?>" alt="Product Image">
         <?php endif; ?>
 
-        <!-- 评论表单 -->
         <h2>Comment Area</h2>
-        <form method="post" action="detail.php">
-            <label for="comment_name">Name:</label>
+        <form method="post" action="detail.php?id=<?php echo $product_id; ?>">
+            <label for="comment_name">Name (optional):</label>
             <input type="text" id="comment_name" name="comment_name" value="<?php echo !empty($error) && isset($_SESSION['saved_input']['comment_name']) ? htmlspecialchars($_SESSION['saved_input']['comment_name']) : ''; ?>">
-
             <label for="comment_text">Comment:</label>
             <textarea id="comment_text" name="comment_text" required><?php echo !empty($error) && isset($_SESSION['saved_input']['comment_text']) ? htmlspecialchars($_SESSION['saved_input']['comment_text']) : ''; ?></textarea>
 
-            <!-- 验证码部分 -->
             <label for="captcha">Please enter the captcha:</label>
             <img src="captcha.php" alt="CAPTCHA Image" id="captcha_image">
             <button type="button" id="refresh_captcha">Refresh</button>
@@ -91,7 +74,6 @@ $product = $stmt->fetch(PDO::FETCH_ASSOC);
             <input type="submit" name="submit_comment" value="Submit Comment">
         </form>
 
-        <!-- 显示评论 -->
         <?php
             $commentQuery = "SELECT * FROM Comments WHERE product_id = :product_id AND is_hidden = FALSE ORDER BY comment_date DESC";
             $stmt = $db->prepare($commentQuery);
@@ -108,7 +90,7 @@ $product = $stmt->fetch(PDO::FETCH_ASSOC);
                     echo "<p><strong>Admin:</strong></p>";
                 }
                 echo "<p>" . htmlspecialchars($comment['comment_content']) . "</p>";
-                // 显示评论时间
+                
                 echo "<p class='comment-date'>Commented on: " . htmlspecialchars($comment['comment_date']) . "</p>";
                 echo "</div>";
             }
@@ -124,5 +106,3 @@ $product = $stmt->fetch(PDO::FETCH_ASSOC);
             });
         });
     </script>
-</body>
-</html>
